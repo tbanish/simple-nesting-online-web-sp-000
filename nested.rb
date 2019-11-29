7b2242abac3707c91da954a8bd3da88faf4a2613
Tom Banish <tombanish@gmail.com>
2019-11-29 04:40:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-array = [["LISP", "C"]]
+array = ["LISP", "C"]
 programmer_hash[:yukirhio_matsumoto][:known_for] << "Ruby"
 programmer_hash[:yukirhio_matsumoto][:languages]
 programmer_hash[:dennis_ritchie][:languages] = array
